1dc008d121e43bb5efd811774fef1b7fcbb954e5
Nitish Kumar <59388067+sudonitish@users.noreply.github.com>
2023-04-19 12:15:57 +0530

    Update mySQLconnection.js

--- backend/config/mySQLconnection.js
+++ backend/config/mySQLconnection.js
@@ -2,8 +2,8 @@ const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: 'nitish',
+    user: 'user', //write DB user
+    password: 'password',//your DB password
     database: 'CHIT_CHAT'
 });
 module.exports = connection;
